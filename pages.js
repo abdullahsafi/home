8f99ed0d7414aba622d2c3b5f54f9cbb8889b212
Abdullah Safi <asaf@uni.sydney.edu.au>
2021-04-28 20:19:27 +1000

    inital commit

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/abdullahsafi/abdullahsafi.github.io.git";
 
 ghpages.publish(
   pathname,
